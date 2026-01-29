797ab11a672f3b3bc07f1c8bb7d59683d4ba5fbb
fardin0045 <fardinonik40@gmail.com>
2026-01-29 11:17:13 +0600

    learned the PUT Method

--- GameStore.api/Program.cs
+++ GameStore.api/Program.cs
@@ -33,22 +33,43 @@ List<GameDto> games = [
 app.MapGet("/games", () => games);
 
 // GET /games/1
-app.MapGet("/games/{id}",(int id) => games.Find(game => game.Id == id)).WithName(GetGameEndpointName);
+app.MapGet("/games/{id}", (int id) => games.Find(game => game.Id == id)).WithName(GetGameEndpointName);
 
 // POSt /games
-app.MapPost("/games",(CreateGameDto newGame) =>
+app.MapPost("/games", (CreateGameDto newGame) =>
 {
-    GameDto game = new(
-      games.Count + 1,
-      newGame.Name,
-      newGame.Genre,
-      newGame.Price,
-      newGame.ReleaseDate
-    );
+   GameDto game = new(
+     games.Count + 1,
+     newGame.Name,
+     newGame.Genre,
+     newGame.Price,
+     newGame.ReleaseDate
+   );
 
-    games.Add(game);
-    return Results.CreatedAtRoute(GetGameEndpointName, new {id=game.Id},game);
+   games.Add(game);
+   return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
 });
+
+//PUT /games/1
+app.MapPut("/games/{id}",(int id, UpdateGameDto updatedGame )=>
+{
+   var index = games.FindIndex(game => game.Id == id);
+   if (index == -1)
+   {
+      return Results.NotFound();
+   }
+
+   games[index] = new GameDto(
+      id,
+      updatedGame.Name,
+      updatedGame.Genre,
+      updatedGame.Price,
+      updatedGame.ReleaseDate
+   );
+   return Results.NoContent();
+});
+
+
 
 app.Run();
 
